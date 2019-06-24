df1d870e5ffbc2b0851c50e77b967bfa4f92d1f1
Matt Eilar <meilar@gmail.com>
2019-06-23 20:32:18 -0700

    create a forEach loop to translate array generated from user number into ping pong format

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,19 @@
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+
+var numberSwitch = [];
+
+numbers.forEach(function(number) {
+  if ((number % 3 == 0) && (number % 5 != 0)) {
+    numberSwitch.push("ping");
+  } else if ((number % 5 == 0) && (number % 3 != 0)) {
+    numberSwitch.push("pong");
+  } else if ((number % 3 == 0) && (number % 5 == 0)) {
+    numberSwitch.push("pingpong")
+  } else {
+    numberSwitch.push(number);
+  };
+});
+
 $(document).ready(function() {
   $("form#user-input").submit(function(event) {
     var userNum = parseInt($("input#userNum").val());
@@ -5,7 +21,6 @@ $(document).ready(function() {
     event.preventDefault();
     $(".result").show();
 
-    alert("2nd stage");
     var numbers = [1];
     for (var index = 1; index <= userNum; index += 1) {
       number.push(index);
