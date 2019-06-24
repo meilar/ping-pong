dcf6bc0526d84e91eaa1af98d95e0798777023aa
Matt Eilar <meilar@gmail.com>
2019-06-23 21:04:50 -0700

    fix business logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,31 @@
-var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
-
-var numberSwitch = [];
-
-numbers.forEach(function(number) {
-  if ((number % 3 == 0) && (number % 5 != 0)) {
-    numberSwitch.push("ping");
-  } else if ((number % 5 == 0) && (number % 3 != 0)) {
-    numberSwitch.push("pong");
-  } else if ((number % 3 == 0) && (number % 5 == 0)) {
-    numberSwitch.push("pingpong")
-  } else {
-    numberSwitch.push(number);
-  };
-});
-
 $(document).ready(function() {
+
+  var numbers = []
+  var numberSwitch = [];
+
+  for (var index = 1; index <= userNum; index += 1) {
+    numbers.push(index);
+    alert(index);
+  };
+
+  numbers.forEach(function(number) {
+    if ((number % 3 == 0) && (number % 5 != 0)) {
+      numberSwitch.push("ping");
+    } else if ((number % 5 == 0) && (number % 3 != 0)) {
+      numberSwitch.push("pong");
+    } else if ((number % 3 == 0) && (number % 5 == 0)) {
+      numberSwitch.push("pingpong")
+    } else {
+      numberSwitch.push(number);
+    };
+  });
+
   $("form#user-input").submit(function(event) {
     var userNum = parseInt($("input#userNum").val());
     alert("User number is " + userNum);
+    alert("output array" + numberSwitch);
     event.preventDefault();
     $(".result").show();
-
-    var numbers = [1];
-    for (var index = 1; index <= userNum; index += 1) {
-      number.push(index);
-      alert(index);
-    };
   });
   $("button#reset").click(function(event) {
     location.reload();
